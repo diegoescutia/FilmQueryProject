ed4a8796044500c01b32dadf81e2defd68b3956f
diegoescutia <escutia.diego@gmail.com>
2022-07-29 16:35:48 -0400

    initial

---
+++ src/com/skilldistillery/filmquery/entities/Actor.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.filmquery.entities;
+
+public class Actor {
+private int id;
+private String firstName;
+private String lastName;
+
+}
---
+++ src/com/skilldistillery/filmquery/entities/Film.java
@@ -0,0 +1,126 @@
+package com.skilldistillery.filmquery.entities;
+
+public class Film {
+	private int id;
+	private String title;
+	private String description;
+	private int releaseYear;
+	private String languageId;
+	private int rentalDuration;
+	private double rentalRate;
+	private int length;
+	private double replacementCost;
+	private String rating;
+	private String specialFeatures;
+	
+	public Film() {
+		
+	}
+	
+	
+	
+
+	public Film(int id, String title, String description, int releaseYear, String languageId, int rentalDuration,
+			double rentalRate, int length, double replacementCost, String rating, String specialFeatures) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.releaseYear = releaseYear;
+		this.languageId = languageId;
+		this.rentalDuration = rentalDuration;
+		this.rentalRate = rentalRate;
+		this.length = length;
+		this.replacementCost = replacementCost;
+		this.rating = rating;
+		this.specialFeatures = specialFeatures;
+	}
+
+
+
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getReleaseYear() {
+		return releaseYear;
+	}
+	public void setReleaseYear(int releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+	public String getLanguageId() {
+		return languageId;
+	}
+	public void setLanguageId(String languageId) {
+		this.languageId = languageId;
+	}
+	public int getRentalDuration() {
+		return rentalDuration;
+	}
+	public void setRentalDuration(int rentalDuration) {
+		this.rentalDuration = rentalDuration;
+	}
+	public double getRentalRate() {
+		return rentalRate;
+	}
+	public void setRentalRate(double rentalRate) {
+		this.rentalRate = rentalRate;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public double getReplacementCost() {
+		return replacementCost;
+	}
+	public void setReplacementCost(double replacementCost) {
+		this.replacementCost = replacementCost;
+	}
+	public String getRating() {
+		return rating;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+	public String getSpecialFeatures() {
+		return specialFeatures;
+	}
+	public void setSpecialFeatures(String specialFeatures) {
+		this.specialFeatures = specialFeatures;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Film [id=").append(id).append(", title=").append(title).append(", description=")
+				.append(description).append(", releaseYear=").append(releaseYear).append(", languageId=")
+				.append(languageId).append(", rentalDuration=").append(rentalDuration).append(", rentalRate=")
+				.append(rentalRate).append(", length=").append(length).append(", replacementCost=")
+				.append(replacementCost).append(", rating=").append(rating).append(", specialFeatures=")
+				.append(specialFeatures).append("]");
+		return builder.toString();
+	}
+		
+	
+}
+  //
